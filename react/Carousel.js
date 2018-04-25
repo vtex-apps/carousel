4de3e8a6b4bb1093f39df1a7022ff3e49544d88c
Felipe Sales <sales.felipeb@gmail.com>
2018-04-25 16:51:22 -0300

    Add dynamic schema to generate multiple banners

--- react/Carousel.js
+++ react/Carousel.js
@@ -3,9 +3,43 @@ import PropTypes from 'prop-types'
 import Slider from 'react-slick'
 import shortid from 'shortid'
 import Spinner from '@vtex/styleguide/lib/Spinner'
+import { range, map, clone, indexBy, prop } from 'ramda'
 
 import Banner from './Banner'
 import { Arrow, Dots } from '@vtex/slick-components'
+
+const bannerStructure = {
+  type: 'object',
+  title: 'banner',
+  properties: {
+    image: {
+      type: 'string',
+      title: 'Banner image',
+    },
+    mobileImage: {
+      type: 'string',
+      title: 'Banner mobile image',
+    },
+    page: {
+      type: 'string',
+      title: 'Banner link',
+    },
+    description: {
+      type: 'string',
+      title: 'Banner description',
+    },
+    targetParams: {
+      type: 'object',
+      title: 'Banner target params',
+      properties: {
+        params: {
+          type: 'string',
+          title: 'Params',
+        },
+      },
+    },
+  },
+}
 
 /**
  * Carousel component. Shows a serie of banners;
@@ -16,6 +50,72 @@ class Carousel extends Component {
 
     this.state = {
       loading: true,
+    }
+  }
+
+  static getSchema = ({ numberOfBanners }) => {
+    numberOfBanners = numberOfBanners ? numberOfBanners : 3
+
+    const getRepeatedProperties = repetition =>
+      indexBy(
+        prop('title'),
+        map(index => {
+          const property = clone(bannerStructure)
+          property.title = `${property.title}${index}`
+          return property
+        }, range(1, repetition + 1))
+      )
+
+    const generatedSchema =
+      numberOfBanners && getRepeatedProperties(numberOfBanners)
+
+    return {
+      title: 'Carousel',
+      description:
+        'A simple carousel component that shows a serie of banners with images and links',
+      type: 'object',
+      properties: {
+        autoplay: {
+          type: 'boolean',
+          title: 'Autoplay',
+          default: true,
+        },
+        showDots: {
+          type: 'boolean',
+          title: 'Show dots',
+          default: true,
+        },
+        showArrows: {
+          type: 'boolean',
+          title: 'Show arrows',
+          default: true,
+        },
+        height: {
+          type: 'number',
+          title: 'Banner max height size (px)',
+          default: 420,
+          enum: [420, 440],
+        },
+        mobileHeight: {
+          type: 'number',
+          title: 'Banner max height size on mobile (px)',
+          default: 339,
+          enum: [339, 159],
+        },
+        autoplaySpeed: {
+          type: 'number',
+          title: 'Autoplay speed(sec):',
+          default: 5,
+          enum: [4, 5, 6],
+        },
+        numberOfBanners: {
+          type: 'number',
+          title: 'Number of banners',
+          default: 3,
+          enum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        },
+        ...generatedSchema,
+      },
     }
   }
 
@@ -53,11 +153,26 @@ class Carousel extends Component {
       banner4,
       banner5,
       banner6,
+      banner7,
+      banner8,
+      banner9,
+      banner10,
     } = this.props
     const { loading } = this.state
     const settings = this.configureSettings()
 
-    const banners = [banner1, banner2, banner3, banner4, banner5, banner6]
+    const banners = [
+      banner1,
+      banner2,
+      banner3,
+      banner4,
+      banner5,
+      banner6,
+      banner7,
+      banner8,
+      banner9,
+      banner10,
+    ]
 
     return (
       <div className="vtex-carousel">
@@ -97,240 +212,6 @@ class Carousel extends Component {
   }
 }
 
-Carousel.schema = {
-  title: 'Carousel',
-  description:
-    'A simple carousel component that shows a serie of banners with images and links',
-  type: 'object',
-  properties: {
-    autoplay: {
-      type: 'boolean',
-      title: 'Autoplay',
-      default: true,
-    },
-    showDots: {
-      type: 'boolean',
-      title: 'Show dots',
-      default: true,
-    },
-    showArrows: {
-      type: 'boolean',
-      title: 'Show arrows',
-      default: true,
-    },
-    height: {
-      type: 'number',
-      title: 'Banner max height size (px)',
-      default: 420,
-      enum: [420, 440],
-    },
-    mobileHeight: {
-      type: 'number',
-      title: 'Banner max height size on mobile (px)',
-      default: 339,
-      enum: [339, 159],
-    },
-    autoplaySpeed: {
-      type: 'number',
-      title: 'Autoplay speed(sec):',
-      default: 5,
-      enum: [4, 5, 6],
-    },
-    banner1: {
-      type: 'object',
-      title: 'Banner 1',
-      properties: {
-        image: {
-          type: 'string',
-          title: 'Banner 1 image',
-        },
-        mobileImage: {
-          type: 'string',
-          title: 'Banner 1 mobile image',
-        },
-        page: {
-          type: 'string',
-          title: 'Banner 1 link',
-        },
-        description: {
-          type: 'string',
-          title: 'Banner 1 description',
-        },
-        targetParams: {
-          type: 'object',
-          title: 'Banner 1 target params',
-          properties: {
-            params: {
-              type: 'string',
-              title: 'Params',
-            },
-          },
-        },
-      },
-    },
-    banner2: {
-      type: 'object',
-      title: 'Banner 2',
-      properties: {
-        image: {
-          type: 'string',
-          title: 'Banner 2 image',
-        },
-        mobileImage: {
-          type: 'string',
-          title: 'Banner 2 mobile image',
-        },
-        page: {
-          type: 'string',
-          title: 'Banner 2 link',
-        },
-        description: {
-          type: 'string',
-          title: 'Banner 2 description',
-        },
-        targetParams: {
-          type: 'object',
-          title: 'Banner 2 target params',
-          properties: {
-            params: {
-              type: 'string',
-              title: 'Params',
-            },
-          },
-        },
-      },
-    },
-    banner3: {
-      type: 'object',
-      title: 'Banner 3',
-      properties: {
-        image: {
-          type: 'string',
-          title: 'Banner 3 image',
-        },
-        mobileImage: {
-          type: 'string',
-          title: 'Banner 3 mobile image',
-        },
-        page: {
-          type: 'string',
-          title: 'Banner 3 link',
-        },
-        description: {
-          type: 'string',
-          title: 'Banner 3 description',
-        },
-        targetParams: {
-          type: 'object',
-          title: 'Banner 3 target params',
-          properties: {
-            params: {
-              type: 'string',
-              title: 'Params',
-            },
-          },
-        },
-      },
-    },
-    banner4: {
-      type: 'object',
-      title: 'Banner 4',
-      properties: {
-        image: {
-          type: 'string',
-          title: 'Banner 4 image',
-        },
-        mobileImage: {
-          type: 'string',
-          title: 'Banner 4 mobile image',
-        },
-        page: {
-          type: 'string',
-          title: 'Banner 4 link',
-        },
-        description: {
-          type: 'string',
-          title: 'Banner 4 description',
-        },
-        targetParams: {
-          type: 'object',
-          title: 'Banner 4 target params',
-          properties: {
-            params: {
-              type: 'string',
-              title: 'Params',
-            },
-          },
-        },
-      },
-    },
-    banner5: {
-      type: 'object',
-      title: 'Banner 5',
-      properties: {
-        image: {
-          type: 'string',
-          title: 'Banner 5 image',
-        },
-        mobileImage: {
-          type: 'string',
-          title: 'Banner 5 mobile image',
-        },
-        page: {
-          type: 'string',
-          title: 'Banner 5 link',
-        },
-        description: {
-          type: 'string',
-          title: 'Banner 5 description',
-        },
-        targetParams: {
-          type: 'object',
-          title: 'Banner 5 target params',
-          properties: {
-            params: {
-              type: 'string',
-              title: 'Params',
-            },
-          },
-        },
-      },
-    },
-    banner6: {
-      type: 'object',
-      title: 'Banner 6',
-      properties: {
-        image: {
-          type: 'string',
-          title: 'Banner 6 image',
-        },
-        mobileImage: {
-          type: 'string',
-          title: 'Banner 6 mobile image',
-        },
-        page: {
-          type: 'string',
-          title: 'Banner 6 link',
-        },
-        description: {
-          type: 'string',
-          title: 'Banner 6 description',
-        },
-        targetParams: {
-          type: 'object',
-          title: 'Banner 6 target params',
-          properties: {
-            params: {
-              type: 'string',
-              title: 'Params',
-            },
-          },
-        },
-      },
-    },
-  },
-}
-
 const bannerProptype = PropTypes.shape({
   image: PropTypes.string,
   mobileImage: PropTypes.string,
@@ -340,6 +221,15 @@ const bannerProptype = PropTypes.shape({
     params: PropTypes.string,
   }),
 })
+
+Carousel.defaultProps = {
+  autoplay: true,
+  showArrows: true,
+  showDots: true,
+  height: 420,
+  mobileHeight: 339,
+  autoplaySpeed: 5,
+}
 
 Carousel.propTypes = {
   /** Should change images automatically */
@@ -354,6 +244,8 @@ Carousel.propTypes = {
   showDots: PropTypes.bool,
   /** Set visibility of arrows */
   showArrows: PropTypes.bool,
+  /** Banner's quantity */
+  numberOfBanners: PropTypes.number,
   /** Banners that will be displayed by the Carousel
    *    image - The image url of the banner
    *    page - The page that the banner will be linking to
@@ -365,6 +257,10 @@ Carousel.propTypes = {
   banner4: bannerProptype,
   banner5: bannerProptype,
   banner6: bannerProptype,
+  banner7: bannerProptype,
+  banner8: bannerProptype,
+  banner9: bannerProptype,
+  banner10: bannerProptype,
 }
 
 export default Carousel
