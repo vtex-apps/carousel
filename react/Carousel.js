bc798390996888cb9e9f120b0375314e45ba01c3
Guilherme Rodrigues <gadr90@gmail.com>
2018-05-01 16:42:59 -0300

    Refactor Carousel component

--- react/Carousel.js
+++ react/Carousel.js
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types'
 import Slider from 'react-slick'
 import Spinner from '@vtex/styleguide/lib/Spinner'
 import { Arrow, Dots } from '@vtex/slick-components'
-import shortid from 'shortid'
 import keyBy from 'lodash/keyBy'
 import map from 'lodash/map'
 import range from 'lodash/range'
 import property from 'lodash/property'
+import {NoSSR} from 'render'
 
 import Banner from './Banner'
 
 import './global.css'
 
 const GLOBAL_PAGES = global.__RUNTIME__ && Object.keys(global.__RUNTIME__.pages)
+
+const DEFAULT_NUM_BANNERS = 3
 
 const bannerProperties = {
   image: {
@@ -35,17 +37,72 @@ const bannerProperties = {
     enum: ['internal', 'external'],
   },
 }
+
+const bannerProptype = PropTypes.shape({
+  image: PropTypes.string,
+  mobileImage: PropTypes.string,
+  description: PropTypes.string,
+  typeOfRoute: PropTypes.string,
+  page: PropTypes.string,
+  params: PropTypes.string,
+})
+
+const defaultBannerProps = index => ({
+  image: `https://raw.githubusercontent.com/vtex-apps/carousel/master/images/banners-0${index + 1}.png`,
+  mobileImage: `https://raw.githubusercontent.com/vtex-apps/carousel/master/images/banners-mobile-0${index + 1}.png`,
+  page: 'store/home',
+  description: 'banner'
+})
+
 /**
  * Carousel component. Shows a serie of banners.
  */
-class Carousel extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      loading: true,
-    }
+export default class Carousel extends Component {
+  static defaultProps = {
+    autoplay: true,
+    showArrows: true,
+    showDots: true,
+    height: 420,
+    mobileHeight: 339,
+    autoplaySpeed: 5,
+    banner0: defaultBannerProps(0),
+    banner1: defaultBannerProps(1),
+    banner2: defaultBannerProps(2),
+    numberOfBanners: DEFAULT_NUM_BANNERS,
   }
+
+  static propTypes = {
+    /** Should change images automatically */
+    autoplay: PropTypes.bool.isRequired,
+    /** How long it should wait to change the banner in secs */
+    autoplaySpeed: PropTypes.number.isRequired,
+    /** Max height size of the banners */
+    height: PropTypes.number.isRequired,
+    /** Max height size of the banners on mobile */
+    mobileHeight: PropTypes.number.isRequired,
+    /** Set visibility of dots */
+    showDots: PropTypes.bool,
+    /** Set visibility of arrows */
+    showArrows: PropTypes.bool,
+    /** Banner's quantity */
+    numberOfBanners: PropTypes.number,
+    /** Banners that will be displayed by the Carousel
+     *    image - The image url of the banner
+     *    page - The page that the banner will be linking to
+     *    description - The description of the image
+     */
+    banner0: bannerProptype,
+    banner1: bannerProptype,
+    banner2: bannerProptype,
+    banner3: bannerProptype,
+    banner4: bannerProptype,
+    banner5: bannerProptype,
+    banner6: bannerProptype,
+    banner7: bannerProptype,
+    banner8: bannerProptype,
+    banner9: bannerProptype,
+  }
+
   static uiSchema = {
     numberOfBanners: {
       'ui:widget': 'range',
@@ -84,10 +141,10 @@ class Carousel extends Component {
 
     const getRepeatedProperties = repetition =>
       keyBy(
-        map(range(1, repetition + 1), index => {
+        map(range(0, repetition), index => {
           const typeOfRoute = props[`banner${index}`] && props[`banner${index}`].typeOfRoute
           return {
-            title: `Banner ${index}`,
+            title: `Banner #${index+1}`,
             key: `banner${index}`,
             type: 'object',
             properties: {
@@ -173,127 +230,53 @@ class Carousel extends Component {
     }
   }
 
-  componentDidMount() {
-    this.setState({ loading: false })
-  }
-
   render() {
     const {
       height,
       mobileHeight,
-      banner1,
-      banner2,
-      banner3,
-      banner4,
-      banner5,
-      banner6,
-      banner7,
-      banner8,
-      banner9,
-      banner10,
+      numberOfBanners,
     } = this.props
-    const { loading } = this.state
     const settings = this.configureSettings()
 
-    const banners = [
-      banner1,
-      banner2,
-      banner3,
-      banner4,
-      banner5,
-      banner6,
-      banner7,
-      banner8,
-      banner9,
-      banner10,
-    ]
+    const banners = []
+    for (let i = 0; i < numberOfBanners; i++) {
+      banners.push(this.props[`banner${i}`])
+    }
+
+    const banner = this.props.banner0
+
+    const fallback = (
+      <div style={{ maxHeight: `${height}px` }}>
+        <Banner
+          image={banner.image}
+          mobileImage={banner.mobileImage}
+          description={banner.description}
+          mobileHeight={mobileHeight}
+          page={banner.page}
+          params={banner.params}
+        />
+      </div>
+    )
 
     return (
       <div className="vtex-carousel">
-        {!loading && (
+        <NoSSR onSSR={fallback}>
           <Slider {...settings}>
-            {banners.map(banner => {
-              if (banner && banner.image) {
-                return (
-                  <div
-                    key={shortid.generate()}
-                    style={{ maxHeight: `${height}px` }}
-                  >
-                    <Banner
-                      image={banner.image}
-                      mobileImage={banner.mobileImage}
-                      description={banner.description}
-                      mobileHeight={mobileHeight}
-                      page={banner.page}
-                      params={banner.params}
-                    />
-                  </div>
-                )
-              }
-            })}
+            {banners.map((banner, i) => banner && banner.image && (
+              <div key={i} style={{ maxHeight: `${height}px` }}>
+                <Banner
+                  image={banner.image}
+                  mobileImage={banner.mobileImage}
+                  description={banner.description}
+                  mobileHeight={mobileHeight}
+                  page={banner.page}
+                  params={banner.params}
+                />
+              </div>
+            ))}
           </Slider>
-        )}
-
-        {loading && (
-          <div className="flex justify-around pa7">
-            <div className="w3">
-              <Spinner />
-            </div>
-          </div>
-        )}
+        </NoSSR>
       </div>
     )
   }
 }
-
-const bannerProptype = PropTypes.shape({
-  image: PropTypes.string,
-  mobileImage: PropTypes.string,
-  description: PropTypes.string,
-  typeOfRoute: PropTypes.string,
-  page: PropTypes.string,
-  params: PropTypes.string,
-})
-
-Carousel.defaultProps = {
-  autoplay: true,
-  showArrows: true,
-  showDots: true,
-  height: 420,
-  mobileHeight: 339,
-  autoplaySpeed: 5,
-}
-
-Carousel.propTypes = {
-  /** Should change images automatically */
-  autoplay: PropTypes.bool.isRequired,
-  /** How long it should wait to change the banner in secs */
-  autoplaySpeed: PropTypes.number.isRequired,
-  /** Max height size of the banners */
-  height: PropTypes.number.isRequired,
-  /** Max height size of the banners on mobile */
-  mobileHeight: PropTypes.number.isRequired,
-  /** Set visibility of dots */
-  showDots: PropTypes.bool,
-  /** Set visibility of arrows */
-  showArrows: PropTypes.bool,
-  /** Banner's quantity */
-  numberOfBanners: PropTypes.number,
-  /** Banners that will be displayed by the Carousel
-   *    image - The image url of the banner
-   *    page - The page that the banner will be linking to
-   *    description - The description of the image
-   */
-  banner1: bannerProptype,
-  banner2: bannerProptype,
-  banner3: bannerProptype,
-  banner4: bannerProptype,
-  banner5: bannerProptype,
-  banner6: bannerProptype,
-  banner7: bannerProptype,
-  banner8: bannerProptype,
-  banner9: bannerProptype,
-  banner10: bannerProptype,
-}
-
-export default Carousel
