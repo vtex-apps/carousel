6213bba1813b0d4420bcb04ce2e4f1acc4fb5f95
Claudivan Filho <claudivan.filho@splab.ufcg.edu.br>
2018-05-16 10:58:15 -0300

    Fix tests

---
+++ react/__mocks__/vtex.storecomponents/Slider.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+/**
+ * Link Mocked Component.
+ */
+export default class Slider extends Component {
+  render() {
+    return this.props.children
+  }
+}
