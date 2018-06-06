49516d6b1bdadc2a30c07ef4cc24c2aff4972c01
Bruno Dias <bruno.dias@vtex.com.br>
2018-06-06 17:21:53 -0300

    Update Slider mock structure to use destructure (#25)

--- react/__mocks__/vtex.store-components.js
+++ react/__mocks__/vtex.store-components.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 /**
  * Link Mocked Component.
  */
-export default class Slider extends Component {
+export class Slider extends Component {
   render() {
     return this.props.children
   }
