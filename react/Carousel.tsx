8c4e397ef20917fe212b0ae5a3b051250357e12b
g-lefundes <gabriel.lefundes@vtex.com.br>
2019-01-04 13:47:01 -0200

    same as before

--- react/Carousel.tsx
+++ react/Carousel.tsx
@@ -178,7 +178,7 @@ export default class Carousel extends Component<Props> {
           {banners.map(
             (banner, i) =>
               banner &&
-              (banner.mobileImage || banner.image) && (
+              (banner.mobileImage || banner.desktopImage) && (
                 <div key={i} style={{ maxHeight: `${height}px` }}>
                   <Banner height={height} {...banner} />
                 </div>
