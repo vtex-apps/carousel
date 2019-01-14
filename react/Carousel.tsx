f3a291761fe99a0d34316ae0b87c62361c2c4a3b
Bruno Dias <bruno.dias@vtex.com.br>
2019-01-14 15:48:53 -0200

    Change Slider CSS tokens

--- react/Carousel.tsx
+++ react/Carousel.tsx
@@ -207,7 +207,7 @@ export default class Carousel extends Component<Props> {
     }
 
     return (
-      <div className="vtex-carousel force-full-width">
+      <div className="vtex-carousel">
         <Slider sliderSettings={settings} leftArrowClasses="ml3 ml5-m ml8-l ml9-xl" rightArrowClasses="mr3 mr5-m mr8-l mr9-xl">
           {banners.filter(banner => banner && (banner.mobileImage || banner.image)).map(
             (banner, i) => (
